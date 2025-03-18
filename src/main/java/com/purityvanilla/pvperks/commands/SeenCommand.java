3544f5e0c53456e9f9fef0f75907cb3c59203e50
chiralpenguin <50334616+chiralpenguin@users.noreply.github.com>
2025-03-18 11:05:26 +0000

    Replace getOfflinePlayer call with internal API method

--- src/main/java/com/purityvanilla/pvperks/commands/SeenCommand.java
+++ src/main/java/com/purityvanilla/pvperks/commands/SeenCommand.java
@@ -5,13 +5,13 @@ import com.purityvanilla.pvcore.util.CustomTagResolvers;
 import com.purityvanilla.pvlib.commands.CommandGuard;
 import com.purityvanilla.pvlib.util.TimeFormatting;
 import com.purityvanilla.pvperks.PVPerks;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Timestamp;
+import java.util.UUID;
 
 public class SeenCommand implements CommandExecutor {
     private final PVPerks plugin;
@@ -24,16 +24,16 @@ public class SeenCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (CommandGuard.argsSizeInvalid(1, args, sender, plugin.config().getMessage("player-seen-usage"))) return true;
 
-        // Make sure to use this method when getting player by username as blocking web request will be made with getOfflinePlayer()
-        OfflinePlayer player = plugin.getServer().getOfflinePlayerIfCached(args[0].toLowerCase());
-        if (player == null || player.getName() == null) {
+        String username = args[0].toLowerCase();
+        UUID uuid = PVCore.getAPI().getPlayerAPI().getUUIDFromName(username);
+        if (uuid == null) {
             sender.sendMessage(plugin.config().getMessage("player-not-found"));
             return true;
         }
 
-        Timestamp lastSeen = PVCore.getAPI().getPlayerAPI().getPlayerLastSeen(player.getUniqueId());
+        Timestamp lastSeen = PVCore.getAPI().getPlayerAPI().getPlayerLastSeen(uuid);
         String dateString = TimeFormatting.basicDateString(lastSeen);
-        sender.sendMessage(plugin.config().getMessage("player-seen", CustomTagResolvers.playerDateResolver(player.getName(), dateString)));
+        sender.sendMessage(plugin.config().getMessage("player-seen", CustomTagResolvers.playerDateResolver(username, dateString)));
         return true;
     }
 }
