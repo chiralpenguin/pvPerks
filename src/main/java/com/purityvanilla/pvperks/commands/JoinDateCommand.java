7349fff9305ced7c6895239a0f0b7f12b14a370d
chiralpenguin <50334616+chiralpenguin@users.noreply.github.com>
2025-05-07 17:18:13 +0100

    Allow joindate and playtime to be used on offline players

--- src/main/java/com/purityvanilla/pvperks/commands/JoinDateCommand.java
+++ src/main/java/com/purityvanilla/pvperks/commands/JoinDateCommand.java
@@ -1,5 +1,6 @@
 package com.purityvanilla.pvperks.commands;
 
+import com.purityvanilla.pvcore.PVCore;
 import com.purityvanilla.pvcore.util.CustomTagResolvers;
 import com.purityvanilla.pvlib.commands.CommandGuard;
 import com.purityvanilla.pvlib.util.TimeFormatting;
@@ -7,6 +8,7 @@ import com.purityvanilla.pvperks.PVPerks;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,6 +16,7 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Timestamp;
+import java.util.UUID;
 
 public class JoinDateCommand implements CommandExecutor {
     private final PVPerks plugin;
@@ -24,7 +27,7 @@ public class JoinDateCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        Player target;
+        OfflinePlayer target;
 
         // Assume player is checking their own joindate if no arguments given
         if (args.length < 1) {
@@ -38,15 +41,17 @@ public class JoinDateCommand implements CommandExecutor {
             return true;
         }
 
-        target = plugin.getServer().getPlayer(args[0].toLowerCase());
-        if (target == null) {
+        UUID targetUUID = PVCore.getAPI().getPlayerAPI().getUUIDFromName(args[0].toLowerCase());
+        if (targetUUID == null) {
             sender.sendMessage(plugin.config().getMessage("player-not-found"));
             return true;
         }
 
+        target = plugin.getServer().getOfflinePlayer(targetUUID);
+        String targetName = PVCore.getAPI().getPlayerAPI().getPlayerUsername(targetUUID);
         Timestamp firstJoined = new Timestamp(target.getFirstPlayed());
         String dateString = TimeFormatting.basicDateString(firstJoined);
-        sender.sendMessage(plugin.config().getMessage("joindate-others", CustomTagResolvers.playerDateResolver(target.getName(), dateString)));
+        sender.sendMessage(plugin.config().getMessage("joindate-others", CustomTagResolvers.playerDateResolver(targetName, dateString)));
         return true;
     }
 }
--- src/main/java/com/purityvanilla/pvperks/commands/PlaytimeCommand.java
+++ src/main/java/com/purityvanilla/pvperks/commands/PlaytimeCommand.java
@@ -1,17 +1,20 @@
 package com.purityvanilla.pvperks.commands;
 
+import com.purityvanilla.pvcore.PVCore;
 import com.purityvanilla.pvlib.commands.CommandGuard;
 import com.purityvanilla.pvlib.util.TimeFormatting;
 import com.purityvanilla.pvperks.PVPerks;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class PlaytimeCommand implements CommandExecutor {
     private final PVPerks plugin;
@@ -22,12 +25,12 @@ public class PlaytimeCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        Player target;
+        OfflinePlayer target;
 
         // Assume player is checking own playtime if no argument given
         if (args.length < 1) {
             if (CommandGuard.senderNotPlayer(sender, plugin.config().getMessage("playtime-usage"))) return true;
-            target = (Player) sender;
+            target = (OfflinePlayer) sender;
             long playtimeSeconds = getPlayerPlaytimeSeconds(target);
             String playtimeString = TimeFormatting.PrettyDurationString(playtimeSeconds);
 
@@ -36,24 +39,26 @@ public class PlaytimeCommand implements CommandExecutor {
             return true;
         }
 
-        target = plugin.getServer().getPlayer(args[0].toLowerCase());
-        if (target == null) {
+        UUID targetUUID = PVCore.getAPI().getPlayerAPI().getUUIDFromName(args[0].toLowerCase());
+        if (targetUUID == null) {
             sender.sendMessage(plugin.config().getMessage("player-not-found"));
             return true;
         }
 
+        target = plugin.getServer().getOfflinePlayer(targetUUID);
+        String targetName = PVCore.getAPI().getPlayerAPI().getPlayerUsername(targetUUID);
         long playTimeSeconds = getPlayerPlaytimeSeconds(target);
         String playTimeString = TimeFormatting.PrettyDurationString(playTimeSeconds);
 
         TagResolver resolver = TagResolver.resolver(
-                Placeholder.component("player", Component.text(target.getName())),
+                Placeholder.component("player", Component.text(targetName)),
                 Placeholder.component("playtime", Component.text(playTimeString))
         );
         sender.sendMessage(plugin.config().getMessage("playtime-others", resolver));
         return true;
     }
 
-    private long getPlayerPlaytimeSeconds(Player player) {
+    private long getPlayerPlaytimeSeconds(OfflinePlayer player) {
         return player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
     }
 }
